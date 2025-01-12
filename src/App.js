c5c6c46c5430412cfd5869ffa9012360a9b7f7ac
subhashreesai <subhashreesamal68@gmail.com>
2025-01-12 10:50:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from './components/Header';
+//import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>welcome</div>
-  )
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+     
+    </>
+  );
 }
- 
-export default App
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    
+  return (
+    <Footer>
+   <h1>welcome</h1>
+    </Footer>
+  );
+};
+
+export default Footer;
